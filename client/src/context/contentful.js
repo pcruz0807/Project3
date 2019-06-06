0cf05023f4c6ed479c136627ee5a67f3ecdc57ae
pcruz0807 <codecruz86@gmail.com>
2019-06-06 15:48:34 -0700

    fix heroku

--- client/src/context/contentful.js
+++ client/src/context/contentful.js
@@ -2,7 +2,7 @@ import * as contentful from "contentful";
 
 export const client = contentful.createClient({
   // This is the space ID. A space is like a project folder in Contentful terms
-  space: process.env.REACT_APP_SPACE_ID,
+  space: "llsr7385zo1w",
   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-  accessToken: process.env.REACT_APP_ACCESS_TOKEN
+  accessToken: "DvsZcYZ3hx8tfpPKs4vI4sTQm0jF-BT2-mTwpjc33xQ"
 });
