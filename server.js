35bceba55c6f68ce80b4f3876a94efa3cc8e1d91
pcruz0807 <codecruz86@gmail.com>
2019-06-06 01:47:59 -0700

    more fixing

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, "/client/public/index.html"));
 });
 app.listen(port);
